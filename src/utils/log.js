d4c34c96a29186cd3ebeef13668d452be2de41b6
Angel Rodríguez <angelmanuelrs@gmail.com>
2019-05-18 11:48:10 -0400

    [NOTICKET] - Improve log util, re-enable reconnect strategy

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,7 +1,13 @@
 const chalk = require('chalk');
 
 const currentTime = () => chalk.gray(`> ${(new Date()).toGMTString()} | `);
-const object = obj => JSON.stringify(obj, null, 2);
+const object = (obj) => {
+  if (typeof obj === 'object') {
+    return JSON.stringify(obj, null, 2);
+  }
+
+  return obj;
+};
 
 const message = (msg) => {
   console.log(currentTime() + chalk.white(msg));
@@ -15,12 +21,20 @@ const info = (msg) => {
   console.log(currentTime() + chalk.bold.blue(msg));
 };
 
-const warning = (msg) => {
-  console.log(currentTime() + chalk.yellow(msg));
+const warning = (msg, obj = null) => {
+  let _msg = `${currentTime()} ${chalk.bold.yellow(msg)}`;
+  if (obj) {
+    _msg += `${chalk.bold.yellow(object(obj))}`;
+  }
+  console.log(_msg);
 };
 
-const error = (msg) => {
-  console.log(currentTime() + chalk.bold.red(msg));
+const error = (msg, obj = null) => {
+  let _msg = `${currentTime()} ${chalk.bold.red(msg)}`;
+  if (obj) {
+    _msg += `${chalk.bold.red(object(obj))}`;
+  }
+  console.log(_msg);
 };
 
 module.exports = {
